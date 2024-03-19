5b1219798af6f256da0dcad6c49e42419d3b5c83
aarrasseayoub01 <aarrasseayoub@gmail.com>
2024-03-19 17:16:16 +0000

    Cloud AI interface (first draft) / dynamic username
    Former-commit-id: 8bf88de0d9a540b0f46fd3ff09cf6d65f75a12c7

--- src/main/java/com/dbn/connection/config/ui/AICloudSettingsForm.java
+++ src/main/java/com/dbn/connection/config/ui/AICloudSettingsForm.java
@@ -1,6 +1,7 @@
 package com.dbn.connection.config.ui;
 
 import com.dbn.common.options.ui.ConfigurationEditorForm;
+import com.dbn.connection.ConnectionHandler;
 import com.dbn.connection.config.AICloudSettings;
 import com.intellij.openapi.options.ConfigurationException;
 
@@ -8,8 +9,37 @@ import javax.swing.*;
 
 public class AICloudSettingsForm extends ConfigurationEditorForm<AICloudSettings> {
 
-  public AICloudSettingsForm(final AICloudSettings aiCloudSettings){
+  private JPanel mainPanel;
+  private JLabel intro;
+  private JLabel networkAllow;
+  private JComboBox providerComboBox;
+  private JTextArea aclTextArea;
+  private JTextArea grantTextArea;
+  private JLabel intro2;
+  private JButton copyButton;
+  private JButton applyButton;
+  private JLabel grantTextField;
+
+  // Variable to hold the user name, replacing the hard-coded "SCOTT"
+  private String userName;
+
+  public AICloudSettingsForm(final AICloudSettings aiCloudSettings) {
     super(aiCloudSettings);
+    this.userName = ConnectionHandler.get(getConfiguration().getConnectionId()).getUserName();
+
+    providerComboBox.addItem("OpenAI");
+    providerComboBox.addItem("Cohere");
+
+    intro.setText("This section displays some mandatory actions that need to be made on the server side for the use of this connection.");
+    intro2.setText("Please perform the following logged as admin to this remote database.");
+
+    grantTextField.setText("Grant execution to user " + userName + " for the AI Cloud Package.");
+
+    grantTextArea.setText("grant execute on DBMS_CLOUD to " + userName + "; \ngrant execute on DBM$_CLOUD_AI to " + userName + ";");
+
+    networkAllow.setText("Allow Network Access to AI Provider");
+
+    aclTextArea.setText("BEGIN\n    DBMS_NETWORK_ACL_ADMIN.APPEND_HOST_ACE(\n       HOST => 'api.cohere.ai',\n       ACE => XS$ACE_TYPE(PRIVILEGE_LIST => XS$NAME_LIST('http'),\n         PRINCIPAL_NAME =>'" + userName + "',\n\tPRINCIPAL_TYPE => XS_ACL.PTYPE_DB));\nEND;\n/");
   }
 
   @Override
@@ -24,6 +54,6 @@ public class AICloudSettingsForm extends ConfigurationEditorForm<AICloudSettings
 
   @Override
   protected JComponent getMainComponent() {
-    return new JPanel();
+    return mainPanel;
   }
 }
