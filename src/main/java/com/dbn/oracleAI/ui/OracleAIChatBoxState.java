6540b72d9ac50a935a6721078af414faf18a24c0
Emmanuel Jannetti <4819607+ejannett@users.noreply.github.com>
2024-04-03 15:31:33 +0200

    add progress bar
    Former-commit-id: e8aafa8a3e9c908483f92b54aaf6adefdbf2328e

--- src/main/java/com/dbn/oracleAI/ui/OracleAIChatBoxState.java
+++ src/main/java/com/dbn/oracleAI/ui/OracleAIChatBoxState.java
@@ -3,11 +3,12 @@ package com.dbn.oracleAI.ui;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.jdom.Element;
 
 import java.util.List;
 
-@Setter @Getter @Builder
+@Setter @Getter @Builder @ToString
 public class OracleAIChatBoxState extends Element {
   //TODO : why we are also an Element ? if we have to why we need OracleAIChatBoxState subclass ?
   private List<OracleAIChatBox.AIProfileItem> profiles;
