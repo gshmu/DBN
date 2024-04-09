4441adedbdaa30b9239431bfabe3a988079eb613
aarrasseayoub01 <aarrasseayoub@gmail.com>
2024-04-09 16:12:46 +0000

    - Deleting Credential - Creating Credential
    Former-commit-id: ba297c24d5162a5a7e2beafda1ce5f1e56632326

--- src/main/java/com/dbn/oracleAI/config/CredentialProvider.java
+++ src/main/java/com/dbn/oracleAI/config/CredentialProvider.java
@@ -1,33 +1,61 @@
 package com.dbn.oracleAI.config;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class CredentialProvider implements AttributeInput {
   protected String credentialName;
   protected String username;
+  protected String password;
+  protected String userOCID;
+  protected String userTenancyOCID;
+  protected String privateKey;
+  protected String fingerprint;
   protected List<Profile> profiles = new ArrayList<>();
-
-  public CredentialProvider(String credentialName) {
-    this.credentialName = credentialName;
-  }
-  public CredentialProvider(String credentialName, String username) {
-    this.credentialName = credentialName;
-    this.username = username;
-  }
 
   @Override
   public boolean isValid() {
     return credentialName != null && !credentialName.contains("'");
   }
 
+  /**
+   * This is to add these attribute in the corresponding pl/sql call
+   */
   @Override
   public String format(){
-    return "";
+    if (!isValid()) {
+      throw new IllegalArgumentException("Invalid credential attributes.");
+    }
+    if(username != null){
+
+      return String.format(
+          "credential_name => '%s',\n" +
+              "username => '%s',\n" +
+              "password => '%s'",
+          credentialName,
+          username,
+          password
+      );
+    } else {
+      return String.format(
+          "credential_name => '%s', \n" +
+              "user_ocid => '%s', \n" +
+              "tenancy_ocid => '%s' \n" +
+              "private_key => '%s' \n" +
+              "fingerprint => '%s'",
+          credentialName,
+          userOCID,
+          userTenancyOCID,
+          privateKey,
+          fingerprint
+      );
+
+    }
+
   };
 }
--- src/main/java/com/dbn/oracleAI/config/ui/CredentialManagementPanel.java
+++ src/main/java/com/dbn/oracleAI/config/ui/CredentialManagementPanel.java
@@ -1,5 +1,6 @@
 package com.dbn.oracleAI.config.ui;
 
+import com.dbn.common.util.Messages;
 import com.dbn.connection.ConnectionHandler;
 import com.dbn.oracleAI.AICredentialService;
 import com.dbn.oracleAI.DatabaseOracleAIManager;
@@ -10,6 +11,8 @@ import com.intellij.util.ui.JBUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -17,38 +20,39 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
- * A panel for managing AI credentials within the application. It provides functionalities
- * to view, edit, and delete AI credentials associated with a specific connection. This UI component
- * is part of the Oracle AI integration module, allowing users to efficiently manage their AI service
- * credentials directly within the IDE.
- *
- * The panel is dynamically populated with credential information retrieved from the AI credential service,
- * and it utilizes the {@link ConnectionHandler} to fetch and manage credentials for a specific project connection.
+ * A panel for managing AI credentials within the application, offering functionalities
+ * to view, edit, and delete AI credentials associated with a specific connection. This component
+ * is part of the Oracle AI integration module, enabling users to manage their AI service
+ * credentials directly within the IDE environment.
+ * <p>
+ * The panel dynamically populates with credential information retrieved from the AI credential service,
+ * leveraging the {@link ConnectionHandler} to fetch and manage credentials for a given project connection.
  */
 public class CredentialManagementPanel extends JPanel {
 
-  static private final ResourceBundle messages =
-      ResourceBundle.getBundle("Messages", Locale.getDefault());
+  static private final ResourceBundle messages = ResourceBundle.getBundle("Messages", Locale.getDefault());
 
   private JPanel mainPane;
   private JList<String> credentialList;
   private JPanel displayInfo;
   private JLabel profilesLabel;
   private JButton deleteButton;
-  private JButton button1;
+  private JButton addButton;
   private JButton editButton;
   private final AICredentialService credentialSvc;
+  private final ConnectionHandler currConnection;
   private Map<String, CredentialProvider> credentialsProvidersMap;
 
   /**
    * Initializes a new instance of the CredentialManagementPanel for managing AI credentials,
    * setting up UI components and fetching credentials for the given connection.
    *
-   * @param connection The ConnectionHandler associated with this panel. It is used to fetch
-   *                   and manage credentials related to the project's Oracle AI integration.
+   * @param connection The ConnectionHandler associated with this panel, used for fetching
+   *                   and managing credentials related to the project's Oracle AI integration.
    */
   public CredentialManagementPanel(ConnectionHandler connection) {
     this.credentialSvc = connection.getProject().getService(DatabaseOracleAIManager.class).getCredentialService();
+    this.currConnection = connection;
     initializeUI();
     fetchCredentialsProviders();
 
@@ -56,16 +60,53 @@ public class CredentialManagementPanel extends JPanel {
   }
 
   /**
-   * Initializes UI components of the panel, setting up list selection listeners for credential selection,
-   * and configuring the appearance of the list and its cells. This method is responsible for the initial
-   * UI setup and layout of the credential management panel.
+   * Initializes UI components of the panel, including setting up list selection listeners for credential selection,
+   * configuring the appearance of the list and its cells, and initializing action listeners for add and delete buttons.
+   * This method is responsible for the initial UI setup and layout of the credential management panel.
    */
   private void initializeUI() {
+
+    // Initializes addButton with its action listener for creating new credential
+    addButton.addActionListener((e) -> {
+      CredentialCreationWindow credCreationWindow = new CredentialCreationWindow(currConnection, credentialSvc);
+
+      //TODO the following will refresh the credentials list whenever we close the creation window, we need to find a way to only run this when we actually manager to create a new credential
+      credCreationWindow.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosed(WindowEvent e) {
+          super.windowClosed(e);
+          fetchCredentialsProviders();
+        }
+      });
+      credCreationWindow.display();
+
+    });
+
+    // Initializes deleteButton with its action listener for deleting selected credentials
+    deleteButton.addActionListener(e -> {
+      Messages.showQuestionDialog(currConnection.getProject(),
+          messages.getString("ai.settings.credential.deletion.title"),
+          messages.getString("ai.settings.credential.deletion.message.prefix") + credentialList.getSelectedValue(),
+          Messages.options(
+              messages.getString("ai.messages.yes"),
+              messages.getString("ai.messages.no")), 1,
+          option -> {
+            if (option == 0) {
+              removeCredential(credentialList.getSelectedValue());
+              fetchCredentialsProviders();
+            }
+          });
+    });
+
+    // Configures credentialList with a list selection listener for updating display info based on selected credential
     credentialList.addListSelectionListener((e) -> {
-      CredentialProvider selectedCredentialProvider = credentialsProvidersMap.get(credentialList.getSelectedValue());
-      displayInfo.removeAll();
-      panelTemplate(selectedCredentialProvider.getCredentialName(), selectedCredentialProvider.getUsername());
-      profilesLabel.setText(selectedCredentialProvider.getProfiles().stream().map(Profile::getProfileName).collect(Collectors.joining(", ")));
+      if (!e.getValueIsAdjusting() && credentialList.getSelectedValue() != null) {
+        CredentialProvider selectedCredentialProvider = credentialsProvidersMap.get(credentialList.getSelectedValue());
+        displayInfo.removeAll();
+        panelTemplate(selectedCredentialProvider.getCredentialName(), selectedCredentialProvider.getUsername());
+        if(selectedCredentialProvider.getProfiles()!=null){
+        profilesLabel.setText(selectedCredentialProvider.getProfiles().stream().map(Profile::getProfileName).collect(Collectors.joining(", ")));
+      }}
     });
     credentialList.setCellRenderer(new DefaultListCellRenderer() {
       @Override
@@ -76,6 +117,16 @@ public class CredentialManagementPanel extends JPanel {
       }
     });
   }
+
+  /**
+   * Removes a specified credential by name and updates the local cache of credentials.
+   *
+   * @param credential The name of the credential to be removed.
+   */
+  private void removeCredential(String credential) {
+    credentialSvc.deleteCredential(credential).thenRun(() -> credentialsProvidersMap.remove(credential));
+  }
+
   /**
    * Asynchronously fetches the list of credential providers from the AI credential service and updates
    * the UI components accordingly. This method retrieves the credentials, updating the credential list
@@ -98,6 +149,7 @@ public class CredentialManagementPanel extends JPanel {
    */
   private void updateCredentialList() {
     credentialList.setListData(credentialsProvidersMap.values().stream().map(CredentialProvider::getCredentialName).toArray(String[]::new));
+    credentialList.setSelectedIndex(0);
   }
 
   /**
@@ -106,12 +158,10 @@ public class CredentialManagementPanel extends JPanel {
    * to show detailed information for the selected credential, including its name and associated username.
    *
    * @param credentialName The name of the credential to display information for.
-   * @param username The username associated with the selected credential.
+   * @param username       The username associated with the selected credential.
    */
   public void panelTemplate(String credentialName, String username) {
-
     GridBagConstraints constraints = new GridBagConstraints();
-
     constraints.fill = GridBagConstraints.HORIZONTAL;
     constraints.anchor = GridBagConstraints.WEST;
     constraints.weightx = 1.0;
@@ -119,16 +169,12 @@ public class CredentialManagementPanel extends JPanel {
     constraints.gridy = GridBagConstraints.RELATIVE;
     constraints.insets = JBUI.insets(2);
 
-
     displayInfo.add(new JLabel(messages.getString("ai.settings.credentials.info.credential_name")), constraints);
     constraints.gridx = 1;
     JTextField credentialNameField = new JTextField(credentialName);
     displayInfo.add(credentialNameField, constraints);
 
-
     constraints.gridx = 0;
-
-
     displayInfo.add(new JLabel(messages.getString("ai.settings.credentials.info.username")), constraints);
     constraints.gridx = 1;
     JTextField userNameField = new JTextField(username);
@@ -137,6 +183,4 @@ public class CredentialManagementPanel extends JPanel {
     displayInfo.revalidate();
     displayInfo.repaint();
   }
-
-
 }
