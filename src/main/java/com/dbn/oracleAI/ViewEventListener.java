6de4fbbcbebd4acec1207d7fa71a110a1c0740c8
Emmanuel Jannetti <4819607+ejannett@users.noreply.github.com>
2024-04-15 12:00:19 +0200

    fix javadoc
    Former-commit-id: 7a0636ec62603180d709ea67b479327a4d0ee0e8

--- src/main/java/com/dbn/oracleAI/ViewEventListener.java
+++ src/main/java/com/dbn/oracleAI/ViewEventListener.java
@@ -2,6 +2,12 @@ package com.dbn.oracleAI;
 
 import java.util.EventListener;
 
+/**
+ * Eventing interface for wizard view change.
+ */
 public interface ViewEventListener extends EventListener {
+  /**
+   * this method is called whenever the view move to one step forward or backward.
+   */
   void notifyViewChange();
 }
--- src/main/java/com/dbn/oracleAI/WizardStep.java
+++ src/main/java/com/dbn/oracleAI/WizardStep.java
@@ -2,8 +2,21 @@ package com.dbn.oracleAI;
 
 import javax.swing.JPanel;
 
+/**
+ * Wizard step interface.
+ */
 public interface WizardStep {
+  /**
+   * Gets the title of this step.
+   * @return the title of the step to be displayed
+   */
   public String getTitle();
+
+  /**
+   * Gets the UI panel for the step.
+   *
+   * @return the step panel
+   */
   public JPanel getViewPort();
 
 }
