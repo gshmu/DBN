6422cc938a324fa4f5480ba81aef8d4d6ce29e82
aarrasseayoub01 <aarrasseayoub@gmail.com>
2024-04-24 10:09:38 +0100

    - Remove old configuration code - Stretch Table of ai provider info - Giving more space for password column - Remove hardcoded strings - Make button to get providers smaller and add tooltip to it - Improve naming of files
    Former-commit-id: 003f2f9bea1d5fa776e0de36e18683489659a902

--- src/main/java/com/dbn/oracleAI/config/AIProviders/ui/AIProviderEditorTable.java
+++ src/main/java/com/dbn/oracleAI/config/AIProviders/ui/AIProviderEditorTable.java
@@ -9,6 +9,8 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.ListSelectionModel;
 import javax.swing.table.TableColumn;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class AIProviderEditorTable extends DBNEditableTable<AIProviderTypesTableModel> {
 
@@ -20,8 +22,12 @@ public class AIProviderEditorTable extends DBNEditableTable<AIProviderTypesTable
     setCellSelectionEnabled(true);
     setDefaultRenderer(String.class, new AIProviderTypesTableCellRenderer());
 
-    setFixedWidth(columnModel.getColumn(2), 100);
-
+    addComponentListener(new ComponentAdapter() {
+      @Override
+      public void componentResized(ComponentEvent e) {
+        adjustColumnSizes();
+      }
+    });
   }
 
   @NotNull
@@ -29,16 +35,28 @@ public class AIProviderEditorTable extends DBNEditableTable<AIProviderTypesTable
     return new AIProviderTypesTableModel(project, environmentTypes);
   }
 
-  void setEnvironmentTypes(AIProviderTypeBundle environmentTypes) {
-    super.setModel(createModel(getProject(), environmentTypes));
-    setFixedWidth(columnModel.getColumn(2), 100);
-    setFixedWidth(columnModel.getColumn(3), 100);
-    setFixedWidth(columnModel.getColumn(4), 60);
+  private void adjustColumnSizes() {
+    int tableWidth = getWidth();
+
+    // Ensure that the table has columns before adjusting sizes
+    if (getColumnModel().getColumnCount() > 2) {
+      TableColumn column1 = getColumnModel().getColumn(0);
+      TableColumn column2 = getColumnModel().getColumn(1);
+      TableColumn column3 = getColumnModel().getColumn(2);
+
+      // Set first two columns to 1/5 of the table each, and third column to 2/5 of the table
+      int column1and2Width = tableWidth / 5;
+      int column3Width = 2 * column1and2Width;
+
+      column1.setPreferredWidth(column1and2Width);
+      column2.setPreferredWidth(column1and2Width);
+      column3.setPreferredWidth(column3Width);
+    }
   }
 
-  private void setFixedWidth(TableColumn tableColumn, int width) {
-    tableColumn.setMaxWidth(width);
-    tableColumn.setMinWidth(width);
+  void setEnvironmentTypes(AIProviderTypeBundle environmentTypes) {
+    super.setModel(createModel(getProject(), environmentTypes));
+    adjustColumnSizes();
   }
 
   @Override
