058eef62a393f4aedd7a35e973d4393a64a4b57a
Emmanuel Jannetti <4819607+ejannett@users.noreply.github.com>
2024-04-30 11:25:01 +0200

    fix layouts fix provider combos
    Former-commit-id: f9d1e596e9ff2bb4ba561dad0cebff66e892cb2d

--- src/main/java/com/dbn/oracleAI/types/ProviderType.java
+++ src/main/java/com/dbn/oracleAI/types/ProviderType.java
@@ -1,15 +1,43 @@
 package com.dbn.oracleAI.types;
 
-import lombok.Getter;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * This enum is for listing the possible credential providers we have
  */
-@Getter
 public enum ProviderType {
+ // first one is the default
+  COHERE(ProviderModel.COMMAND,
+         ProviderModel.COMMAND_NIGHTLY,
+         ProviderModel.COMMAND_LIGHT,
+         ProviderModel.COMMAND_LIGHT_NIGHTLY),
+  OPENAI(ProviderModel.GPT_3_5_TURBO,
+          ProviderModel.GPT_3_5_TURBO_0613,
+          ProviderModel.GPT_3_5_TURBO_16K,
+          ProviderModel.GPT_3_5_TURBO_16K_0613,
+          ProviderModel.GPT_4,
+          ProviderModel.GPT_4_0613,
+          ProviderModel.GPT_4_32K,
+          ProviderModel.GPT_4_32K_0613),
+  OCI(ProviderModel.COHERE_COMMAND,
+          ProviderModel.COHERE_COMMAND_LIGHT,
+          ProviderModel.META_LLAMA_2_70B_CHAT,
+          ProviderModel.COHERE_EMBED_ENGLISH_LIGHT_V2_0,
+          ProviderModel.COHERE_EMBED_ENGLISH_LIGHT_V2_0,
+          ProviderModel.COHERE_EMBED_ENGLISH_V3_0,
+          ProviderModel.COHERE_EMBED_ENGLISH_LIGHT_V3_0,
+          ProviderModel.COHERE_EMBED_MULTILINGUAL_V3_0,
+          ProviderModel.COHERE_EMBED_MULTILINGUAL_LIGHT_V3_0);
 
-  COHERE,
-  OPENAI,
-  OCI;
+  private Set models = Collections.emptySet();
+  ProviderType(ProviderModel ... models) {
+    this.models = new HashSet<>(List.of(models));
+  }
 
+ public Set<ProviderModel> getModels() {
+  return models;
+ }
 }
