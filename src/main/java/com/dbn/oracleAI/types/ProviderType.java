e9c45e414b8b62dda803b4aeaa06fc7ad6a0b271
Emmanuel Jannetti <4819607+ejannett@users.noreply.github.com>
2024-05-02 09:28:22 +0200

    impl fake service for testing
    Former-commit-id: e094edf4d5bbcf44a5cd497d9a84d7fa1cfc49d7

--- src/main/java/com/dbn/oracleAI/types/ProviderType.java
+++ src/main/java/com/dbn/oracleAI/types/ProviderType.java
@@ -1,20 +1,17 @@
 package com.dbn.oracleAI.types;
 
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
  * This enum is for listing the possible credential providers we have
  */
 public enum ProviderType {
- // first one is the default
-  COHERE(ProviderModel.COMMAND,
+  COHERE(ProviderModel.COMMAND, ProviderModel.COMMAND,
          ProviderModel.COMMAND_NIGHTLY,
          ProviderModel.COMMAND_LIGHT,
          ProviderModel.COMMAND_LIGHT_NIGHTLY),
-  OPENAI(ProviderModel.GPT_3_5_TURBO,
+  OPENAI(ProviderModel.GPT_3_5_TURBO, ProviderModel.GPT_3_5_TURBO,
           ProviderModel.GPT_3_5_TURBO_0613,
           ProviderModel.GPT_3_5_TURBO_16K,
           ProviderModel.GPT_3_5_TURBO_16K_0613,
@@ -22,10 +19,9 @@ public enum ProviderType {
           ProviderModel.GPT_4_0613,
           ProviderModel.GPT_4_32K,
           ProviderModel.GPT_4_32K_0613),
-  OCI(ProviderModel.COHERE_COMMAND,
+  OCI(ProviderModel.COHERE_COMMAND, ProviderModel.COHERE_COMMAND,
           ProviderModel.COHERE_COMMAND_LIGHT,
           ProviderModel.META_LLAMA_2_70B_CHAT,
-          ProviderModel.COHERE_EMBED_ENGLISH_LIGHT_V2_0,
           ProviderModel.COHERE_EMBED_ENGLISH_LIGHT_V2_0,
           ProviderModel.COHERE_EMBED_ENGLISH_V3_0,
           ProviderModel.COHERE_EMBED_ENGLISH_LIGHT_V3_0,
@@ -33,11 +29,17 @@ public enum ProviderType {
           ProviderModel.COHERE_EMBED_MULTILINGUAL_LIGHT_V3_0);
 
   private Set models = Collections.emptySet();
-  ProviderType(ProviderModel ... models) {
-    this.models = new HashSet<>(List.of(models));
+  private ProviderModel defaultModel;
+  ProviderType(ProviderModel defaultModel, ProviderModel ... models) {
+   this.models = Set.of(models);
+   this.defaultModel = defaultModel;
   }
 
  public Set<ProviderModel> getModels() {
   return models;
  }
+ public ProviderModel getDefaultModel() {
+  return defaultModel;
+ }
+
 }
