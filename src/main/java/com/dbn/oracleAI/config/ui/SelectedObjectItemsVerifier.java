71975c542441f49d06282475a7153975fd257de2
Ayoub Aarrasse <ayoub.aarrasse@oracle.com>
2024-05-15 11:25:47 +0100

    Fixing/Improving validation for profile configuration
    Former-commit-id: d031152e63c9377d196b80ecd82e93c45d9ecfb8

--- src/main/java/com/dbn/oracleAI/config/ui/SelectedObjectItemsVerifier.java
+++ src/main/java/com/dbn/oracleAI/config/ui/SelectedObjectItemsVerifier.java
@@ -1,10 +1,16 @@
 package com.dbn.oracleAI.config.ui;
 
+import javax.swing.BorderFactory;
 import javax.swing.InputVerifier;
 import javax.swing.JComponent;
 import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Input verifier for an AI profile name
@@ -12,10 +18,16 @@ import java.awt.event.ActionListener;
 public class SelectedObjectItemsVerifier extends InputVerifier implements
     ActionListener {
 
+
+  private static final Border ERROR_BORDER = BorderFactory.createLineBorder(Color.RED, 1);
+  private static final Border DEFAULT_BORDER = UIManager.getBorder("TextField.border");
+  private final ResourceBundle messages = ResourceBundle.getBundle("Messages", Locale.getDefault());
+
+
   @Override
   public void actionPerformed(ActionEvent e) {
     JTable input = (JTable) e.getSource();
-    shouldYieldFocus(input); //ignore return value
+    shouldYieldFocus(input);
   }
 
   @Override
@@ -26,8 +38,16 @@ public class SelectedObjectItemsVerifier extends InputVerifier implements
 
   @Override
   public boolean verify(JComponent input) {
-    // TODO : enough for now
-    return ((JTable) input).getRowCount() > 0;
+    JTable profileTable = (JTable) input;
+    boolean isValid = profileTable.getRowCount() > 0;
+    if (!isValid) {
+      profileTable.setBorder(ERROR_BORDER);
+      profileTable.setToolTipText(messages.getString("profile.mgmt.object_list_step.validation"));
+    } else {
+      profileTable.setBorder(DEFAULT_BORDER);
+      profileTable.setToolTipText(null);
+    }
+    return isValid;
   }
 }
 
