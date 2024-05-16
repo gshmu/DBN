ccb391043092c7eee18ef1ff426e40d8aa4ecd8a
aarrasseayoub01 <aarrasseayoub@gmail.com>
2024-05-16 08:52:06 +0100

    fixes
    Former-commit-id: 9ec49e09e71accf681749c559aa30d88c1f193ef

--- src/main/java/com/dbn/oracleAI/config/AttributeInput.java
+++ src/main/java/com/dbn/oracleAI/config/AttributeInput.java
@@ -6,17 +6,14 @@ public interface AttributeInput {
    * Validates the information by making sure the required attributes are not empty
    * and don't have illegal characters
    */
-  default void validate() {
-  }
+  void validate();
 
-  ;
 
   /**
    * Formats the attributes into a string suitable for PL/SQL calls.
+   *
+   * @param forCreation This is to specify whether the resulting output is for creating a new profile or updating an existing one
    */
-  default String toAttributeMap(boolean isNew) {
-    return "";
-  }
+  String toAttributeMap(boolean forCreation);
 
-  ;
 }
