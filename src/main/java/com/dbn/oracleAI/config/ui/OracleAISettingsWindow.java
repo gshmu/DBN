b5906c4944b870351351c4eddce01accc28dcb6f
aarrasseayoub01 <aarrasseayoub@gmail.com>
2024-05-20 09:47:12 +0100

    Adding help button
    Former-commit-id: 10bfd4e0467784f3fb8cf2e3088966ea9cc102c1

--- src/main/java/com/dbn/oracleAI/config/ui/OracleAISettingsWindow.java
+++ src/main/java/com/dbn/oracleAI/config/ui/OracleAISettingsWindow.java
@@ -8,48 +8,54 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class OracleAISettingsWindow extends JDialog {
 
   static private final ResourceBundle messages =
-    ResourceBundle.getBundle("Messages", Locale.getDefault());
+      ResourceBundle.getBundle("Messages", Locale.getDefault());
 
   private JPanel settingsWindowContentPane;
   private JTabbedPane settingsTabbedPane;
   private JPanel settingsWindowBottomPanel;
   private JButton settingsCloseButton;
-  private ConnectionHandler currConnection;
+  private JButton helpButton;
+  private final ConnectionHandler currConnection;
 
   public OracleAISettingsWindow(ConnectionHandler connection) {
-    super(WindowManager.getInstance().getFrame(connection.getProject()), messages.getString(
-      "ai.settings.window.title"), true);
+    super(WindowManager.getInstance().getFrame(connection.getProject()), messages.getString("ai.settings.window.title"), true);
     currConnection = connection;
     setContentPane(settingsWindowContentPane);
     setTitle(messages.getString("ai.settings.window.title"));
     setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    setupBottomPanel();
     pack();
     setLocationRelativeTo(WindowManager.getInstance().getFrame(connection.getProject()));
     setTabs();
+  }
 
-    settingsCloseButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        dispose();
-      }
+  private void setupBottomPanel() {
+    settingsWindowBottomPanel.setLayout(new BorderLayout());
+
+    settingsCloseButton.addActionListener(e -> dispose());
+
+    helpButton.addActionListener(e -> {
+      AICloudSettingsForm helpDialog = new AICloudSettingsForm(currConnection);
+      helpDialog.showAndGet();
     });
+
+    settingsWindowBottomPanel.add(helpButton, BorderLayout.WEST);
+    settingsWindowBottomPanel.add(settingsCloseButton, BorderLayout.EAST);
   }
-
-  public void display() {
-    setVisible(true);
-  }
-
-
 
   private void setTabs() {
     settingsTabbedPane.addTab(messages.getString("ai.settings.window.tab.profiles.title"), new ProfileManagementPanel(currConnection));
     settingsTabbedPane.addTab(messages.getString("ai.settings.window.tab.credentials.title"), new CredentialManagementPanel(currConnection));
+  }
+
+  public void display() {
+    setVisible(true);
   }
 }
