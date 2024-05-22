ba0f11478c1343f65382822065d1f2158bafc4fb
aarrasseayoub01 <aarrasseayoub@gmail.com>
2024-05-22 10:22:03 +0100

    Adding support for enablement/disablement of credentials in config
    Former-commit-id: 50392ac225a46c027d2e02acfd885bb88ce8eded

--- src/main/java/com/dbn/oracleAI/FakeAICredentialService.java
+++ src/main/java/com/dbn/oracleAI/FakeAICredentialService.java
@@ -12,42 +12,46 @@ import java.util.concurrent.CompletableFuture;
 
 public class FakeAICredentialService implements AICredentialService {
 
-    Type CREDENTIAL_TYPE = new TypeToken<List<Credential>>() {
-    }.getType();
-    String credentialsRepoFilename = System.getProperty("fake.services.credential.dump", "/var/tmp/credentials.json");
-    //keep track of profiles
-    // no synch needed
-    List<Credential> credentials = null;
+  Type CREDENTIAL_TYPE = new TypeToken<List<Credential>>() {
+  }.getType();
+  String credentialsRepoFilename = System.getProperty("fake.services.credential.dump", "/var/tmp/credentials.json");
+  //keep track of profiles
+  // no synch needed
+  List<Credential> credentials = null;
 
 
-    @Override
-    public CompletableFuture<Void> createCredential(Credential credential) {
-        this.credentials.add(credential);
-        return CompletableFuture.completedFuture(null);
+  @Override
+  public CompletableFuture<Void> createCredential(Credential credential) {
+    this.credentials.add(credential);
+    return CompletableFuture.completedFuture(null);
+  }
+
+  @Override
+  public CompletableFuture<Void> updateCredential(Credential credential) {
+    this.credentials.removeIf(cred -> cred.getCredentialName().equalsIgnoreCase(credential.getCredentialName()));
+    this.credentials.add(credential);
+    return CompletableFuture.completedFuture(null);
+  }
+
+  @Override
+  public CompletableFuture<List<Credential>> getCredentials() {
+    if (credentials == null) {
+      try {
+        this.credentials = new Gson().fromJson(new FileReader(credentialsRepoFilename), CREDENTIAL_TYPE);
+      } catch (FileNotFoundException e) {
+        throw new RuntimeException("cannot read credentials  list " + e.getMessage());
+      }
     }
+    return CompletableFuture.completedFuture(this.credentials);
+  }
 
-    @Override
-    public CompletableFuture<Void> updateCredential(Credential credential) {
-        this.credentials.removeIf(cred -> cred.getCredentialName().equalsIgnoreCase(credential.getCredentialName()));
-        this.credentials.add(credential);
-        return CompletableFuture.completedFuture(null);
-    }
+  @Override
+  public CompletableFuture<Void> deleteCredential(String credentialName) {
+    this.credentials.removeIf(cred -> cred.getCredentialName().equalsIgnoreCase(credentialName));
+    return CompletableFuture.completedFuture(null);
+  }
 
-    @Override
-    public CompletableFuture<List<Credential>> getCredentials() {
-        if (credentials == null) {
-            try {
-                this.credentials = new Gson().fromJson(new FileReader(credentialsRepoFilename), CREDENTIAL_TYPE);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException("cannot read credentials  list " + e.getMessage());
-            }
-        }
-        return CompletableFuture.completedFuture(this.credentials);
-    }
-
-    @Override
-    public CompletableFuture<Void> deleteCredential(String credentialName) {
-        this.credentials.removeIf(cred -> cred.getCredentialName().equalsIgnoreCase(credentialName));
-        return CompletableFuture.completedFuture(null);
-    }
+  @Override
+  public void updateStatus(String credentialName, Boolean isEnabled) {
+  }
 }
