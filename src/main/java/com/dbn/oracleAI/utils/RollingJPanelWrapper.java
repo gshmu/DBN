5d713af00a6be50ee89e63006dce4dca86784335
aarrasseayoub01 <aarrasseayoub@gmail.com>
2024-07-08 16:26:14 +0100

    - Tool window name changing - Hiding tool window if not supported - Fixing state handling - Using PsiComment to do initial filtering
    Former-commit-id: 542e847d75e8c2782a87e4ce22481b7f9deb284b

--- src/main/java/com/dbn/oracleAI/utils/RollingJPanelWrapper.java
+++ src/main/java/com/dbn/oracleAI/utils/RollingJPanelWrapper.java
@@ -5,7 +5,7 @@ import com.dbn.oracleAI.ui.ChatMessage;
 import com.dbn.oracleAI.ui.JIMSendTextPane;
 
 import javax.swing.JPanel;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,56 +14,56 @@ import java.util.List;
  */
 public class RollingJPanelWrapper {
 
-    private FixedSizeList<ChatMessage> items;
-    private JPanel messageContainer;
-    private int maxCapacity = -1;
+  private FixedSizeList<ChatMessage> items;
+  private JPanel messageContainer;
+  private int maxCapacity = -1;
 
-    /**
-     * Creates a new RollingJPanelWrapper
-     *
-     * @param maxCapacity max capacity
-     * @param panel       the panel to display the chat message
-     */
-    public RollingJPanelWrapper(int maxCapacity, JPanel panel) {
-        this.maxCapacity = maxCapacity;
-        this.messageContainer = panel;
-        this.items = new FixedSizeList<>(maxCapacity);
+  /**
+   * Creates a new RollingJPanelWrapper
+   *
+   * @param maxCapacity max capacity
+   * @param panel       the panel to display the chat message
+   */
+  public RollingJPanelWrapper(int maxCapacity, JPanel panel) {
+    this.maxCapacity = maxCapacity;
+    this.messageContainer = panel;
+    this.items = new FixedSizeList<>(maxCapacity);
+  }
+
+  private void ensureFreeSlot(int howMany) {
+    int currentSize = items.size();
+    int s = maxCapacity - currentSize - howMany;
+    while (s++ < 0) {
+      this.items.remove(0);
+      this.messageContainer.remove(0);
     }
+  }
 
-    private void ensureFreeSlot(int howMany) {
-        int currentSize = items.size();
-        int s = maxCapacity - currentSize - howMany;
-        while (s++ < 0) {
-            this.items.remove(0);
-            this.messageContainer.remove(0);
-        }
+
+  private JPanel createMessagePane(ChatMessage chatMessage) {
+    JIMSendTextPane messagePane = new JIMSendTextPane();
+    messagePane.setText(chatMessage.getMessage());
+    messagePane.setAuthorColor(chatMessage.getAuthor());
+    return messagePane;
+  }
+
+  public void clear() {
+    this.messageContainer.removeAll();
+    this.items.clear();
+  }
+
+  public void addAll(List<ChatMessage> chatMessages) {
+    ensureFreeSlot(chatMessages.size());
+    for (ChatMessage message : chatMessages) {
+      this.items.add(message);
+      JPanel messagePane = createMessagePane(message);
+      this.messageContainer.add(messagePane, message.getAuthor() == AuthorType.AI ? "wrap, w ::80%" : "wrap, al right, w ::80%");
     }
+    this.messageContainer.revalidate();
+    this.messageContainer.repaint();
+  }
 
-
-    private JPanel createMessagePane(ChatMessage chatMessage) {
-        JIMSendTextPane messagePane = new JIMSendTextPane();
-        messagePane.setText(chatMessage.getMessage());
-        messagePane.setAuthorColor(chatMessage.getAuthor());
-        return messagePane;
-    }
-
-    public void clear() {
-        this.messageContainer.removeAll();
-        this.items.clear();
-    }
-
-    public void addAll(List<ChatMessage> chatMessages) {
-        ensureFreeSlot(chatMessages.size());
-        for (ChatMessage message : chatMessages) {
-            this.items.add(message);
-            JPanel messagePane = createMessagePane(message);
-            this.messageContainer.add(messagePane, message.getAuthor() == AuthorType.AI ? "wrap, w ::80%" : "wrap, al right, w ::80%");
-        }
-        this.messageContainer.revalidate();
-        this.messageContainer.repaint();
-    }
-
-    public List<ChatMessage> getMessages() {
-        return Collections.unmodifiableList(this.items);
-    }
+  public List<ChatMessage> getMessages() {
+    return new ArrayList<>(this.items);
+  }
 }
