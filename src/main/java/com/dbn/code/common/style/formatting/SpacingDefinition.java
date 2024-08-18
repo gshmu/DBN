7b3a5c1149038e66416a6fcb88a7be0f086246db
Dan Cioca <dancioca@bluewin.ch>
2024-08-18 17:54:00 +0200

    DBNE-10158 lazy formatting attributes initialization
    Former-commit-id: 0f0dd1fde77ef9e6b2bdb84fec1baea751db6ed6

--- src/main/java/com/dbn/code/common/style/formatting/SpacingDefinition.java
+++ src/main/java/com/dbn/code/common/style/formatting/SpacingDefinition.java
@@ -4,45 +4,31 @@ import com.dbn.code.common.style.presets.CodeStylePreset;
 import com.intellij.formatting.Spacing;
 import org.jdom.Element;
 
+import java.util.function.Supplier;
+
 import static com.dbn.common.options.setting.Settings.enumAttribute;
 
 public enum SpacingDefinition implements FormattingAttribute<Spacing>{
-    NO_SPACE  (new Loader(){
-        @Override
-        Spacing load(){return CodeStylePreset.SPACING_NO_SPACE;}}),
-    ONE_SPACE (new Loader(){
-        @Override
-        Spacing load(){return CodeStylePreset.SPACING_ONE_SPACE;}}),
-
-    LINE_BREAK (new Loader(){
-        @Override
-        Spacing load(){return CodeStylePreset.SPACING_LINE_BREAK;}}),
-    ONE_LINE  (new Loader(){
-        @Override
-        Spacing load(){return CodeStylePreset.SPACING_ONE_LINE;}}),
-
-    MIN_LINE_BREAK (new Loader(){
-        @Override
-        Spacing load(){return CodeStylePreset.SPACING_MIN_LINE_BREAK;}}),
-    MIN_ONE_LINE  (new Loader(){
-        @Override
-        Spacing load(){return CodeStylePreset.SPACING_MIN_ONE_LINE;}}),
-    MIN_ONE_SPACE  (new Loader(){
-        @Override
-        Spacing load(){return CodeStylePreset.SPACING_MIN_ONE_SPACE;}}),
+    NO_SPACE       (() -> CodeStylePreset.SPACING_NO_SPACE),
+    ONE_SPACE      (() -> CodeStylePreset.SPACING_ONE_SPACE),
+    LINE_BREAK     (() -> CodeStylePreset.SPACING_LINE_BREAK),
+    ONE_LINE       (() -> CodeStylePreset.SPACING_ONE_LINE),
+    MIN_LINE_BREAK (() -> CodeStylePreset.SPACING_MIN_LINE_BREAK),
+    MIN_ONE_LINE   (() -> CodeStylePreset.SPACING_MIN_ONE_LINE),
+    MIN_ONE_SPACE  (() -> CodeStylePreset.SPACING_MIN_ONE_SPACE),
     ;
 
     private Spacing value;
-    private Loader<Spacing> loader;
+    private Supplier<Spacing> loader;
 
-    private SpacingDefinition(Loader<Spacing> loader) {
+    private SpacingDefinition(Supplier<Spacing> loader) {
         this.loader = loader;
     }
 
     @Override
     public Spacing getValue() {
         if (value == null && loader != null) {
-            value = loader.load();
+            value = loader.get();
             loader = null;
         }
         return value;
